591e01865f452ba27d27db467b416551caae491d
freddypauls <paasan-fred@hotmail.com>
2018-05-01 14:03:24 +0200

    What

---
+++ public/js/signOut.js
@@ -0,0 +1,11 @@
+$(".signOutSelector").click(function(){
+		
+	firebase.auth().signOut().then(function() {
+		// Sign-out successful.
+		console.log("You are now logged out");
+	}).catch(function(error) {
+		// An error happened.
+	});
+		
+});
+
