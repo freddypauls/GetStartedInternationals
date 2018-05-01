991632825c8fd63dd869f2abd80400fa72c04788
Henrik Reff Snilsberg <hrsnils@gmail.com>
2018-05-01 12:12:11 +0200

    comfirm password

--- public/js/register.js
+++ public/js/register.js
@@ -1,6 +1,5 @@
-$("#regBtn").click(
-	function(){
-		
+$("#regBtn").click(function(){
+		if(validatePassword()){
 		var email = $("#regEmail").val();
 		var password = $("#regPassword").val();
 		var name = $("#regName").val();
@@ -13,55 +12,40 @@ $("#regBtn").click(
 			firebase.auth().createUserWithEmailAndPassword(email, password)
 			.then(function() {
 
-
-
-
 				firebase.auth().signInWithEmailAndPassword(email, password)
 				.then(function(){
 
 					firebase.database().ref().child('users').once('value', function(snapshot) {
-					if (!snapshot.hasChild(name)) {
-						$uid = firebase.auth().currentUser.uid;
-						console.log('User created!');
-						
-						//puts the new user into the database
-						firebase.database().ref().child('users').child(name).set({
-							uid: $uid,
-							username: name,
-							email: email,
-							userType: userType
-						});
-						
-						$user = firebase.auth().currentUser;
-						
-						$user.updateProfile({
-							displayName: $uname,
-						}).then(function() {
-							// Update successful.
-						}).catch(function(error) {
-							// An error happened.
-						});
-						
-						$username = firebase.auth().currentUser.displayName;
-						console.log($username);
-
-						//takes you to the chat view
-						document.location.href = "index.html";
-					} 
-					else {
-						alert("That user already exists");
-						console.log("User with username: " + $uname + " already exists!")
-					}
-				});
-					
-					var user = firebase.auth().currentUser;
-					
-					user.updateProfile({displayName: name})
-					.then(function() {
-							// Update successful.
+						if (!snapshot.hasChild(name)) {
+							$uid = firebase.auth().currentUser.uid;
+							console.log('User created!');
+							
+							//puts the new user into the database
+							firebase.database().ref().child('users').child(name).set({
+								uid: $uid,
+								username: name,
+								email: email,
+								userType: userType
+							});
+							
+							$user = firebase.auth().currentUser;
+							
+							$user.updateProfile({
+								displayName: name,
+							}).then(function() {
+								// Update successful.
+								$username = firebase.auth().currentUser.displayName;
+								console.log($username);
+							}).catch(function(error) {
+								// An error happened.
+							});
+							
+							//takes you to the indexpage
 							window.location.replace('index.html');
-						}).catch(function(error) {
-							$("#regError").show().text(error.message);
+						} else {
+							alert("That user already exists");
+							console.log("User with username: " + $uname + " already exists!")
+						}
 					});
 				
 				}).catch(function(error){
@@ -74,36 +58,38 @@ $("#regBtn").click(
 				// Handle Errors here.
 				$("#regError").show().text(error.message);					
 			});
-				//window.location.replace('login.html');
 		}
 		
+	}else{
+		$("#regError").show().text('Passwords does not match!');
 	}
+});
+
 
 	/* MULIG VI KAN BRUKE NOE SLIKT FOR Å FIKSE BRUKER NAVN
 	firebase.auth().onAuthStateChanged(function(user) {
 
-                if (user) {
+				if (user) {
 
-                   // Updates the user attributes:
+				   // Updates the user attributes:
 
-                  user.updateProfile({ // <-- Update Method here
+				  user.updateProfile({ // <-- Update Method here
 
-                    displayName: "NEW USER NAME",
-                    photoURL: "https://example.com/jane-q-user/profile.jpg"
+					displayName: "NEW USER NAME",
+					photoURL: "https://example.com/jane-q-user/profile.jpg"
 
-                  }).then(function() {
+				  }).then(function() {
 
-                    // Profile updated successfully!
-                    //  "NEW USER NAME"
+					// Profile updated successfully!
+					//  "NEW USER NAME"
 
-                    var displayName = user.displayName;
-                    // "https://example.com/jane-q-user/profile.jpg"
-                    var photoURL = user.photoURL;
+					var displayName = user.displayName;
+					// "https://example.com/jane-q-user/profile.jpg"
+					var photoURL = user.photoURL;
 
-                  }, function(error) {
-                    // An error happened.
-                  });     
+				  }, function(error) {
+					// An error happened.
+				  });     
 
-                }
-    });*/
-);
+				}
+	});*/
