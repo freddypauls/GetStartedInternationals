3a6b3c27ea492fb4754d590aa1003ed3a38f32e5
Henrik Reff Snilsberg <hrsnils@gmail.com>
2018-05-01 17:19:24 +0200

    reserve item

--- public/js/reserveItem.js
+++ public/js/reserveItem.js
@@ -11,14 +11,34 @@ $reserveButtons.click(function(){
 	});
 });
 
+function reserveItem(item){
+	$itemID = item;
+	console.log($itemID);
 
-function modalChoice(choice){
-	if(choice == true){
+	$("#modalAgree").click(function(){
 		//do reservation
-		console.log(choice);
-		return true;
-	} else {
-		//do nothing
-		return false;
-	}
+		console.log('Agreed to book');
+
+		$database = firebase.database().ref().child('items');
+
+		$database.once('value', function(snapshot) {
+			//checks if the itemID is unique (it should be)
+			if (snapshot.hasChild($itemID)) {
+
+				//puts the new item into the database
+				$database.child($itemID).update({
+					status: 'Reserved',
+					reserved_by: firebase.auth().currentUser.displayName,
+					reserved_by_id: firebase.auth().currentUser.uid,
+				});
+				
+				
+				M.toast({html: 'Item reserved!'})
+			} else {
+
+				M.toast({html: 'That item does not exist!'})
+				console.log("Item does not exist!")
+			}
+		});
+	});
 }
