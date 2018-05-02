27ef4a8e6b32b0b3f15e6dc14072abc6da2994ed
freddypauls <paasan-fred@hotmail.com>
2018-05-02 16:36:28 +0200

    dont print edit or delete on admin

--- public/js/usersAdmin.js
+++ public/js/usersAdmin.js
@@ -5,8 +5,11 @@
 		//On message added
 		firebase.database().ref().child('users').on('child_added', function(snapshot) {
 		 var user = snapshot.val();
-
+			
 		 //creates html elemets to be displayed with database data
+		 var fill = document.createElement("p");
+		 fill.textContent = "";	
+		
 		 var usernameElement = document.createElement("td");
 		 usernameElement.textContent = user.username;
 		 usernameElement.className = "";
@@ -15,32 +18,32 @@
 		 emailElement.textContent = user.userType;
 		 emailElement.className = "";
 
-     var crossButton = document.createElement("i");
-     crossButton.textContent="clear";
-     crossButton.className="tiny material-icons right crossUsersAdmin";
+		 var crossButton = document.createElement("i");
+		 crossButton.textContent="clear";
+		 crossButton.className="tiny material-icons right crossUsersAdmin";
 
-     // Delete user onclick function
-     crossButton.onclick=function delOnClick(){
+		 // Delete user onclick function
+		 crossButton.onclick=function delOnClick(){
 
-       var firebaseRef = firebase.database().ref("users/" + user.username);
-       firebaseRef.remove().then(function(){
-         console.log("removal of user from database succeded " + user.username);
+		   var firebaseRef = firebase.database().ref("users/" + user.username);
+		   firebaseRef.remove().then(function(){
+			 console.log("removal of user from database succeded " + user.username);
 
-         var user = firebase.auth().currentUser;
+			 var user = firebase.auth().currentUser;
 
-            user.delete().then(function() {
-              // User deleted.
-              console.log("removal of user authentication succeded " + user.username);
-              location.reload();
-            }).catch(function(error) {
-              // An error happened.
-              console.log("remove authentication failed: " + error.message);
-            });
-         
-       }).catch(function(error){
-         console.log("remove failed: " + error.message);
-       })
-     };
+				user.delete().then(function() {
+				  // User deleted.
+				  console.log("removal of user authentication succeded " + user.username);
+				  location.reload();
+				}).catch(function(error) {
+				  // An error happened.
+				  console.log("remove authentication failed: " + error.message);
+				});
+
+		   }).catch(function(error){
+			 console.log("remove failed: " + error.message);
+		   })
+		 };
 
       var editButton = document.createElement("a");
       editButton.textContent="edit";
@@ -65,16 +68,20 @@
 
       };
 
-		 var usersElement = document.createElement("tr");
+		var usersElement = document.createElement("tr");
 
-  		usersElement.appendChild(usernameElement);
-      usersElement.appendChild(emailElement);
-      usersElement.appendChild(crossButton);
-      usersElement.appendChild(editButton);
-
+		usersElement.appendChild(usernameElement);
+		usersElement.appendChild(emailElement);
+	 if(user.userType == "Admin"){
+		 usersElement.appendChild(fill);
+		 usersElement.appendChild(fill);
+	 }
+	 else {
+		usersElement.appendChild(crossButton);
+		usersElement.appendChild(editButton);
+	}
       document.getElementById("userList").appendChild(usersElement);
-
-		});
+	});
  }
 
  // Begin listening for data
