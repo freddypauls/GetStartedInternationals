143d39622abc12ea597c84cf31b1d2afe7538089
freddypauls <paasan-fred@hotmail.com>
2018-05-02 16:26:14 +0200

    fix admin index

--- public/js/showItemsAdmin.js
+++ public/js/showItemsAdmin.js
@@ -4,6 +4,23 @@ function addItem(itm) {
 
 	if(itm.status == "Available"){
 			//creates html elemets to be displayed with database data
+		
+			var img = itm.type.replace(/\s+/g, "_");
+	  		
+			var theImage = document.createElement('img');
+			theImage.setAttribute('src', 'images/'+ img +'.jpg');
+			theImage.setAttribute('alt','Image of '+ itm.type);
+			theImage.className="itemImageClass";
+
+			var theItemTitle = document.createElement('span');
+			theItemTitle.className="card-title strokeme";
+			theItemTitle.textContent = itm.name;
+
+			var theItemTop = document.createElement("div");
+			theItemTop.className = "card-image";
+			theItemTop.appendChild(theImage);
+			theItemTop.appendChild(theItemTitle);
+		
 			var itmNameElement = document.createElement("b");
 			itmNameElement.textContent = itm.name;
 			itmNameElement.className="black-text";
@@ -16,32 +33,37 @@ function addItem(itm) {
 			itmTypeElement.textContent = "Type: " + itm.type;
 			itmTypeElement.className="black-text";
 
-      var crossButton = document.createElement("i");
-      crossButton.textContent="clear";
-      crossButton.className="small material-icons right";
+			var crossButton = document.createElement("i");
+			crossButton.textContent="clear";
+			crossButton.className="small material-icons crossBtnStyle";
 
-      //Adds delete function to clear icon
-      crossButton.onclick=function delOnClick(){
-        var firebaseRef = firebase.database().ref("items/" + itm.itemid);
-        firebaseRef.remove().then(function(){
-          console.log("remove suceeded " + itm.itemid);
-          location.reload();
-        }).catch(function(error){
-          console.log("remove failed: " + error.message);
-        })
-      };
+			//Adds delete function to clear icon
+			crossButton.onclick=function delOnClick(){
+			var firebaseRef = firebase.database().ref("items/" + itm.itemid);
+			firebaseRef.remove().then(function(){
+			  console.log("remove suceeded " + itm.itemid);
+			  location.reload();
+			}).catch(function(error){
+			  console.log("remove failed: " + error.message);
+			})
+			};
+		
+			var theItemEnd = document.createElement('div');
+			theItemEnd.className = 'card-action';
+			theItemEnd.appendChild(crossButton);
 
 			var theItmElement = document.createElement("div");
 			theItmElement.appendChild(itmNameElement);
 			theItmElement.appendChild(itmAmountElement);
 			theItmElement.appendChild(itmTypeElement);
-      theItmElement.appendChild(crossButton);
 
 			theItmElement.className = "card-content";
 
 
 			var itmElement = document.createElement("div");
+			itmElement.appendChild(theItemTop);
 			itmElement.appendChild(theItmElement);
+			itmElement.appendChild(theItemEnd);
 
 
 			itmElement.className = "card white card-margin";
